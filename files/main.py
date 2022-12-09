1a53414a3518d75d2050efd931862fd9978e19de
Beatrice398 <65484722+Beatrice398@users.noreply.github.com>
2022-12-09 06:10:47 -0800

    Update main.py

--- files/main.py
+++ files/main.py
@@ -1,53 +1,38 @@
-from tkinter import *
+import tkinter as tk
+# from tkinter import *
 from cell import Cell
 import settings
 import utils
 
 
-root = Tk()
+root = tk.Tk()
 # Override the settings of the window
-root.configure(bg="black")
+root.configure(bg=settings.COLOR)
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
-top_frame = Frame(
-    root,
-    bg='black',
-    width=settings.WIDTH,
-    height=utils.height_prct(25)
+top_frame = tk.Frame(
+    root, bg=settings.COLOR, width=settings.WIDTH, height=utils.height_percentage(25)
 )
 top_frame.place(x=0, y=0)
 
-game_title = Label(
-    top_frame,
-    bg='black',
-    fg='white',
-    text='Minesweeper Game',
-    font=('', 48)
+game_title = tk.Label(
+    top_frame, bg=settings.COLOR, fg='white', text='Minesweeper Game', font=('', 48)
 )
 
-game_title.place(
-    x=utils.width_prct(25), y=0
-)
+game_title.place(x=utils.width_percentage(25), y=0)
 
-left_frame = Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(25),
-    height=utils.height_prct(75)
+left_frame = tk.Frame(
+    root, bg=settings.COLOR, width=utils.width_percentage(25), height=utils.height_percentage(75)
 )
-left_frame.place(x=0, y=utils.height_prct(25))
+left_frame.place(x=0, y=utils.height_percentage(25))
 
-center_frame = Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(75),
-    height=utils.height_prct(75)
+center_frame = tk.Frame(
+    root, bg=settings.COLOR, width=utils.width_percentage(75), height=utils.height_percentage(75)
 )
 center_frame.place(
-    x=utils.width_prct(25),
-    y=utils.height_prct(25),
+    x=utils.width_percentage(25), y=utils.height_percentage(25),
 )
 
 for x in range(settings.GRID_SIZE):
