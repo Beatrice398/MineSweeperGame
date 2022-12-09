40230888f94a4bfa98a2d6bfce0932731d4a3bb3
Beatrice398 <65484722+Beatrice398@users.noreply.github.com>
2022-12-09 06:07:24 -0800

    Update cell.py

--- files/cell.py
+++ files/cell.py
@@ -4,46 +4,40 @@ import settings
 import ctypes
 import sys
 
+
 class Cell:
     all = []
     cell_count = settings.CELL_COUNT
     cell_count_label_object = None
-    def __init__(self,x, y, is_mine=False):
+
+    def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.is_opened = False
         self.is_mine_candidate = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
-
-        # Append the object to the Cell.all list
         Cell.all.append(self)
 
     def create_btn_object(self, location):
-        btn = Button(
-            location,
-            width=12,
-            height=4,
+        button = Button(
+            location, width=12, height=4,
         )
-        btn.bind('<Button-1>', self.left_click_actions ) # Left Click
-        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
-        self.cell_btn_object = btn
+        button.bind('<Button-1>', self.left_click_actions)   # Left Click
+        button.bind('<Button-3>', self.right_click_actions)   # Right Click
+        self.cell_btn_object = button
 
     @staticmethod
     def create_cell_count_label(location):
-        lbl = Label(
-            location,
-            bg='black',
-            fg='white',
-            text=f"Cells Left:{Cell.cell_count}",
-            font=("", 30)
+        label = Label(
+            location, bg=settings.COLOR, fg='white', text=f"Cells Left:{Cell.cell_count}", font=("", 30)
         )
-        Cell.cell_count_label_object = lbl
+        Cell.cell_count_label_object = label
 
-    def left_click_actions(self, event):
+    def left_click_actions(self):
         if self.is_mine:
             self.show_mine()
-        else:
+        if not self.is_mine:
             if self.surrounded_cells_mines_length == 0:
                 for cell_obj in self.surrounded_cells:
                     cell_obj.show_cell()
@@ -56,7 +50,8 @@ class Cell:
         self.cell_btn_object.unbind('<Button-1>')
         self.cell_btn_object.unbind('<Button-3>')
 
-    def get_cell_by_axis(self, x,y):
+    @staticmethod
+    def get_cell_by_axis(x, y):
         # Return a cell object based on the value of x,y
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
@@ -65,7 +60,7 @@ class Cell:
     @property
     def surrounded_cells(self):
         cells = [
-            self.get_cell_by_axis(self.x - 1, self.y -1),
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
             self.get_cell_by_axis(self.x - 1, self.y),
             self.get_cell_by_axis(self.x - 1, self.y + 1),
             self.get_cell_by_axis(self.x, self.y - 1),
@@ -96,8 +91,7 @@ class Cell:
                 Cell.cell_count_label_object.configure(
                     text=f"Cells Left:{Cell.cell_count}"
                 )
-            # If this was a mine candidate, then for safety, we should
-            # configure the background color to SystemButtonFace
+
             self.cell_btn_object.configure(
                 bg='SystemButtonFace'
             )
@@ -110,14 +104,13 @@ class Cell:
         ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine', 'Game Over', 0)
         sys.exit()
 
-
-    def right_click_actions(self, event):
+    def right_click_actions(self):
         if not self.is_mine_candidate:
             self.cell_btn_object.configure(
                 bg='orange'
             )
             self.is_mine_candidate = True
-        else:
+        if self.is_mine_candidate:
             self.cell_btn_object.configure(
                 bg='SystemButtonFace'
             )
